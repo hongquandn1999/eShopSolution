6f4fbf9592115e5dd80630165fe2918587b3eccf
hongquan <Hongquan85@linuxteamvietnam.edu.vn>
2021-03-03 23:48:49 +0700

    Add File Structure

---
+++ eShopSolution.Data/EF/EShopDBContext.cs
@@ -0,0 +1,20 @@
+﻿using eShopSolution.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eShopSolution.Data.EF
+{
+    public class EShopDBContext : DbContext
+    {
+        public EShopDBContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products;
+    }
+}
